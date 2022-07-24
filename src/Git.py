f39830e2b03ec07d672313e228122d9333897713
Matt London <mattlondon57@gmail.com>
2022-07-24 15:59:43 -0400

    Setup git pulling and checking system

--- src/Git.py
+++ src/Git.py
@@ -1,12 +1,67 @@
 """
 Runs all the commands associated with pulling and talking to the repo, checking for changes
 
-Matt
+London
 """
 import os
+import subprocess
 
 
-class GitHandler:
-    def __init__(self, repoLink: str):
+class Git:
+    def __init__(self, repoLink: str, branch: str, storePath: str):
         self.repoLink = repoLink
+        self.branch = branch
+        self.storePath = storePath
 
+        self.setupComplete = False
+
+    def setup_repo(self) -> None:
+        """
+        Setup the repo
+
+        :throws: If setup was unsuccessful
+        """
+        os.system("rm -rf {}".format(self.storePath))
+
+        if os.system("mkdir {}".format(self.storePath)) != 0:
+            raise Exception("Failed to make dir at {}".format(self.storePath))
+
+        os.chdir("{}".format(self.storePath))
+
+        os.system("git clone {} .".format(self.repoLink))
+        os.system("git checkout {}".format(self.branch))
+
+        os.system("git pull")
+
+        self.setupComplete = True
+
+    def test_pull(self) -> bool:
+        """
+        Fetch into the temp location and determine changes
+
+        :return: If there are actually changes
+        """
+        os.chdir("{}".format(self.storePath))
+        hasChanges = subprocess.check_output(["git", "fetch"]).decode() != ""
+
+        return hasChanges
+
+    def update(self) -> bool:
+        """
+        Checks for changes and pulls them if needed
+
+        :return: If there were changes
+        """
+        if not self.setupComplete:
+            self.setup_repo()
+            return False
+
+        if not self.test_pull():
+            return False
+
+        # Otherwise pull changes, this resets as well to make sure the changes are properly pulled
+        os.system("git reset --hard")
+        os.system("git checkout {}".format(self.branch))
+        os.system("git pull --force")
+
+        return True
