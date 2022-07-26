d0351670145697f2e8b89adea46477a2d964b615
Matt London <mattlondon57@gmail.com>
2022-07-26 00:27:20 -0400

    Fix crashing after first recompile

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ from src.Git import Git
 from src.Config import *
 
 import socket
+import os
 
 """ Port to look for webhook call"""
 PORT = 1337
@@ -18,32 +19,36 @@ def main() -> int:
     """
     Handle running on webhook call
     """
-
-    while True:
-        s = socket.socket()
-
-        s.bind((socket.gethostname(), PORT))
-        s.listen(2)
-        conn, _ = s.accept()
-        _ = conn.recv(1024)
-        conn.close()
-
-        # Now execute
-        execute()
-
-    return 0
-
-
-def execute():
-    """
-    Handle running the program
-    """
     cmdList, postCompileBinPath, binFinalLoc = process_compile("cfg/compile.xml")
     repoLink, branch, repoPath = process_git("cfg/git.xml")
 
     gh = Git(repoLink, branch, repoPath)
     ch = Compile(cmdList, repoPath, postCompileBinPath, binFinalLoc)
 
+    headDir = os.getcwd()
+
+    while True:
+        s = socket.socket()
+
+        s.bind((socket.gethostname(), PORT))
+        s.listen(2)
+        print("Waiting for connection...")
+        conn, _ = s.accept()
+        _ = conn.recv(1024)
+        conn.close()
+
+        # Now execute
+        execute(gh, ch)
+
+        os.chdir(headDir)
+
+    return 0
+
+
+def execute(gh, ch):
+    """
+    Handle running the program
+    """
     if gh.update():
         ch.compile()
         print("Compilation done!")
